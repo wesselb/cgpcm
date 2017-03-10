33129dcba307c0e1cb3f7087127ff8dc2043ecdc
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-03-10 18:50:18 +0000

    Separation of training and plotting, and toy experiment

--- src/plots.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python plot_interpolation.py --compute
-python plot_prior.py
-python plot_prior.py --psd
--- src/tasks/toy.py
+++ src/tasks/toy.py
@@ -1,45 +1,74 @@
-from core.learn import Task, TaskConfig
+from core.exp import Task, TaskConfig
 from core.cgpcm import VCGPCM
 import core.data as data
 
 
 class Experiment(Task):
-    def generate_config(self, options):
-        causal = 'causal' in options
-        causal_model = 'causal-model' in options
-        small = 'small' in options
-        fn = 'toy_{}_sample_{}'.format('causal' if causal else 'acausal',
-                                       'cgpcm' if causal_model else 'gpcm')
-        return TaskConfig(name='Toy Experiment',
-                          seed=51,
-                          fn=fn,
-                          iters_pre=400,
-                          iters=2000,
-                          samps=500,
+    """
+    Toy experiment.
 
+    The first option is
+
+    Options:
+
+        - resample: number of times to resample, this option is required and
+                    must be first,
+        - causal: generate a causal sample,
+        - causal-model: use the GPCM instead of the CGPCM, and
+        - post: use posttraining.
+    """
+
+    def generate_config(self, options):
+        # Parse options
+        if len(options) == 0:
+            raise RuntimeError('first option is required')
+        resample = int(options[0])
+        causal = 'causal' in options
+        causal_mod = 'causal-model' in options
+        post = 'post' in options
+
+        fn = 'toy_{}_{}_sample_{}{}'.format(resample,
+                                            'causal' if causal else 'acausal',
+                                            'cgpcm' if causal_mod else 'gpcm',
+                                            '_post' if post else '')
+
+        return TaskConfig(name='Toy Experiment',
+                          seed=85,
+                          fn=fn,
+
+                          # Training options
+                          iters_pre=200,
+                          iters=500,
+                          iters_post=50 if post else 0,
+                          samps=300,
+
+                          # Sample options
                           causal=causal,
-                          causal_model=causal_model,
-                          resample=0,
-                          n=150 if small else 400,
-                          nx=50 if small else 100,
+                          causal_model=causal_mod,
+                          resample=resample,
+                          n=500,
+                          nx=120,
                           nh=31,
-                          k_len=0.1,
-                          k_wiggles=1.5,
-                          data_scale=.5,
-                          noise=1e-4)
+                          k_len=.15,
+                          k_wiggles=2,
+                          noise=1e-4,
+
+                          # Learner options
+                          data_scale=.3,
+                          learner_scale=.6)
 
     def load(self, sess):
         # Load data
-        f, k, h = data.load_akm(sess=sess,
-                                n=self.config.n,
-                                nh=self.config.nh,
-                                k_len=self.config.data_scale
-                                      * self.config.k_len,
-                                k_wiggles=self.config.k_wiggles,
-                                causal=self.config.causal,
-                                resample=self.config.resample)
+        f, k, h, psd = data.load_akm(sess=sess,
+                                     n=self.config.n,
+                                     nh=self.config.nh,
+                                     k_len=self.config.data_scale
+                                           * self.config.k_len,
+                                     k_wiggles=self.config.k_wiggles,
+                                     causal=self.config.causal,
+                                     resample=self.config.resample)
         e = f.make_noisy(self.config.noise)
-        self._set_data(h=h, k=k, f=f, e=e)
+        self._set_data(h=h, k=k, f=f, e=e, psd=psd)
 
         # Construct model
         mod = VCGPCM.from_recipe(sess=sess,
@@ -48,6 +77,6 @@ class Experiment(Task):
                                  nh=self.config.nh,
                                  k_len=self.config.k_len,
                                  k_wiggles=self.config.k_wiggles
-                                           / self.config.data_scale,
+                                           / self.config.learner_scale,
                                  causal=self.config.causal_model)
         self._set_model(mod)
