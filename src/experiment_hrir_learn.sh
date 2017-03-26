c2bfb8d90166ac93d8bd8f0032b41e47d10c1344
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-03-26 05:11:31 +0100

    Fixed HRIR experiment script

--- src/experiment_hrir_learn.sh
+++ src/experiment_hrir_learn.sh
@@ -6,8 +6,8 @@ to_resample=$2
 for (( i = $from_resample; i <= $to_resample; i++ )); do
     ./core.py -t hrir resample $i causal-model \
               -t hrir resample $i \
-              -p compare2 index1 0 index2 0 2side-h no-psd ms mf1 \
-              -p compare2 index1 1 index2 1 2side-h no-psd ms mf1 \
-              -p compare2 index1 0 index2 1 2side-h no-psd ms \
+              -p compare2 index1 0 index2 0 mp no-psd ms mf1 \
+              -p compare2 index1 1 index2 1 mp no-psd ms mf1 \
+              -p compare2 index1 0 index2 1 mp no-psd ms \
               --learn
 done
