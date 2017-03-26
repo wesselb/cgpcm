673758d38007355eb12efdf3d836cfac817a463e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-03-26 04:43:01 +0100

    Updated experiments'

--- src/experiment_toy_learn.sh
+++ src/experiment_toy_learn.sh
@@ -7,10 +7,16 @@ for (( i = $from_resample; i <= $to_resample; i++ )); do
     # Causal sample
     ./core.py -t toy resample $i causal-sample causal-model \
               -t toy resample $i causal-sample \
+              -p compare2 index1 0 index2 0 mf1 \
+              -p compare2 index1 1 index2 1 mf1 \
+              -p compare2 index1 0 index2 1 \
               --learn
 
     # Acausal sample
     ./core.py -t toy resample $i causal-model \
               -t toy resample $i \
+              -p compare2 index1 0 index2 0 mf1 \
+              -p compare2 index1 1 index2 1 mf1 \
+              -p compare2 index1 0 index2 1 \
               --learn
 done
