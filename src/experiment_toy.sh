483377d1d6b0c80b3236b3fac4daa3e98233fd96
Wessel <wessel.p.bruisma@gmail.com>
2017-03-28 16:18:37 +0000

    Merged two calls to core.py into one for toy experiment

--- src/experiment_toy.sh
+++ src/experiment_toy.sh
@@ -5,19 +5,15 @@ to_resample=$2
 opts=${@:3:${#}}
 
 for (( i = $from_resample; i <= $to_resample; i++ )); do
-    # Causal sample
     ./core.py -t toy resample $i causal-sample causal-model \
               -t toy resample $i causal-sample \
-              -p compare index0 0 index1 0 mf0 \
-              -p compare index0 1 index1 1 mf0 \
-              -p compare index0 0 index1 1 \
-              $opts
-
-    # Acausal sample
-    ./core.py -t toy resample $i causal-model \
+              -t toy resample $i causal-model \
               -t toy resample $i \
               -p compare index0 0 index1 0 mf0 \
               -p compare index0 1 index1 1 mf0 \
               -p compare index0 0 index1 1 \
+              -p compare index0 2 index1 2 mf0 \
+              -p compare index0 3 index1 3 mf0 \
+              -p compare index0 2 index1 3 \
               $opts
 done
